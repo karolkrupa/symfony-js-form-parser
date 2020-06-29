9f6260ff4c655e60f37ab96d3e6d7212019f0f08
Karol Krupa <karo2krupa@gmail.com>
2020-06-29 21:16:08 +0200

    types

---
+++ types/ErrorNode.d.ts
@@ -0,0 +1,32 @@
+declare type NodeChildrenObject = {
+    [name: string]: NodeObject;
+};
+export declare type NodeObject = {
+    errors?: string[];
+    children?: NodeChildrenObject;
+};
+export declare type FlattenErrorNode = {
+    [key: string]: string[];
+};
+export declare type SimpleFlattenErrorNode = {
+    [key: string]: string | null;
+};
+export declare class ErrorNode {
+    private _children;
+    readonly name: string;
+    private errors;
+    constructor(node: NodeObject, name?: string | null);
+    hasErrors(): boolean;
+    getErrors(): string[];
+    getName(): string;
+    getFlattenObject(): FlattenErrorNode;
+    getSimpleFlattenObject(): SimpleFlattenErrorNode;
+    get children(): {
+        [p: string]: ErrorNode;
+    };
+    private get rootNode();
+    private parse;
+    private parseErrors;
+    private parseChildren;
+}
+export {};
---
+++ types/ErrorResponse.d.ts
@@ -0,0 +1,32 @@
+import { ErrorNode, FlattenErrorNode, NodeObject, SimpleFlattenErrorNode } from "./ErrorNode";
+declare type FormErrorResponse = {
+    code: Number;
+    message: string;
+    errors: NodeObject;
+};
+declare type FlattenErrors = {
+    code: Number;
+    message: string | null;
+    errors: string[];
+    nodes: FlattenErrorNode;
+};
+declare type SimpleFlattenErrors = {
+    code: Number;
+    message: string | null;
+    error: string | null;
+    nodes: SimpleFlattenErrorNode;
+};
+export declare class ErrorResponse {
+    private readonly _code;
+    private readonly _message;
+    private rootNode;
+    constructor(response: FormErrorResponse);
+    getFlattenObject(): FlattenErrors;
+    getSimpleFlattenObject(): SimpleFlattenErrors;
+    get code(): Number;
+    get message(): string;
+    get children(): {
+        [name: string]: ErrorNode;
+    };
+}
+export {};
